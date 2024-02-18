bbba449d1c2b3de703446eec3448815b2d7ca1c4
Dwamenachrist <agyapongfrancis520@gmail.com>
2024-02-18 00:40:15 +0000

    Done

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
 for i in range(97, 123):
-    if i not in (101, 113):  # Check for ASCII codes of 'e' and 'q' directly
-        print("{}".format(chr(i)), end="")
-
+    if chr(i) not in 'qe':  # Filter out 'q' and 'e' 
+        print(chr(i),
+              end="")
